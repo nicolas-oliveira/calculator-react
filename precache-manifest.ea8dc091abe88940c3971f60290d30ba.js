cfcc6e0ec4d9d5aad4fb2004dbc800c27eaf8b98
Nicolas <nicolasm15@hotmail.com>
2020-05-10 16:52:27 -0300

    Create deploy for repo and code folder

---
+++ precache-manifest.ea8dc091abe88940c3971f60290d30ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c491711774182e6f398da8fbe4a96ae1",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "00bc13c90e57342758e4",
+    "url": "/calculator-react/static/css/main.7c616018.chunk.css"
+  },
+  {
+    "revision": "f231bfb7c93da5d2a38d",
+    "url": "/calculator-react/static/js/2.5f0a4de5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator-react/static/js/2.5f0a4de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00bc13c90e57342758e4",
+    "url": "/calculator-react/static/js/main.9cf94237.chunk.js"
+  },
+  {
+    "revision": "f9d168cf02121f19cddd",
+    "url": "/calculator-react/static/js/runtime-main.ded78f98.js"
+  }
+]);
